60efdb9e07eae3fdbdc596fb2b63d03a731c85f0
Alexandre Severo <alexandre.severo@me.com>
2020-04-16 21:21:06 -0300

    /get implemented

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,24 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const income = transactions.reduce(
+      (prevVal: number, { value, type }: Transaction) => {
+        return prevVal + (type === 'income' ? value : 0);
+      },
+      0,
+    );
+
+    const outcome = transactions.reduce(
+      (prevVal: number, { value, type }: Transaction) => {
+        return prevVal + (type === 'outcome' ? value : 0);
+      },
+      0,
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
